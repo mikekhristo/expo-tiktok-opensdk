c23e6b789c768c1a14992b94a4a0865158c57c4c
Mike Khristo <mikekhristo@gmail.com>
2024-12-19 20:57:26 -0800

    fix: update plugin export and bump version
    - Update app.plugin.js to export plugin function as default
    - Fix plugin resolution in Expo apps
    - Bump version to 0.1.9

--- app.plugin.js
+++ app.plugin.js
@@ -1,4 +1,3 @@
 import withTikTokOpenSDK from './plugin/build/withTikTokOpenSDK.js';
-import ExpoTikTokOpenSDK from './build/index.js';
 
-export { withTikTokOpenSDK, ExpoTikTokOpenSDK };
+export default withTikTokOpenSDK;
